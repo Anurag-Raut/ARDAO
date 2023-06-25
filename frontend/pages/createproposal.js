e4377ab2823eae7fd11fb47f8855499f4f7b2e38
Anurag-Raut <rautab0@gmail.com>
2023-06-25 20:36:12 +0530

    fc

--- frontend/pages/createproposal.js
+++ frontend/pages/createproposal.js
@@ -115,7 +115,7 @@ function CreateProposal() {
         Total Number of Proposals: {totalProposals?.data?.toString()}
       </div>
       <div className='m-2 w-[20vw]'>
-    <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">AR token TokenId</label>
+    <label for="small-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">AR TokenId</label>
         <input type="text"  id="tokenId" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
     </div>
     <div className='m-2 w-[20vw]'>
