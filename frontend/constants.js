6a80e304148df3c26d3e42baa6634fa7b3cef903
Anurag-Raut <rautab0@gmail.com>
2023-06-26 03:52:38 +0530

    deploy-problem-fix

--- frontend/constants.js
+++ frontend/constants.js
@@ -3,10 +3,10 @@
   import NFTMarketplace from './abi/NFTmarketplace.sol/NFTMarketplace.json'
   import Dao from './abi/Dao.sol/DAO.json'
   import DaoToken from './abi/Daotoken.sol/DAOToken.json'
-export const ARTokenAddress = "0x87b234DdAa8c66BF9d6Ce087645Fa50ba2d092aB";
-export const NFTMarketplaceAddress = "0xB9fcAB769F346a785df9dA10FFD363245D8233d6";
-export const DAOAddress = "0x4eBf2A26C7857B9a5Aa3aEafC44b43F982619929";
-export const DAOTokenAddress = "0xd6527558d1C4D572e4CCADa60BB02fC5D0F38cf1";
+export const ARTokenAddress = "0x3ec0F1cA069ab0bbDAbaceF055D66FfD0774C8DD";
+export const NFTMarketplaceAddress = "0xF8e3F17c1281A8D050cD6393968E2143aF7D0132";
+export const DAOAddress = "0x026827870B9cB08cb5b6A755573d746c10ea032B";
+export const DAOTokenAddress = "0xd5CA685AC1CF320FE781D0B69C932Df1e7099783";
 
 
 
